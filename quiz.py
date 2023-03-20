91774f3020544432c149597d08cec9e74b84eda6
Jarosław Kulesza <jkulesza@grupazpr.pl>
2023-03-20 12:23:51 +0100

    coś działa

--- quiz.py
+++ quiz.py
@@ -12,5 +12,26 @@ quiz_uid = sys.argv[1]
 
 quiz_data = [row for row in quizy if row["uid"] == quiz_uid]
 
+questions = {}
 for row in quiz_data:
-    print(row["question"])
+    if not row["question"] in questions:
+        questions[row["question"]] = []
+    questions[row["question"]].append(row["answer"])
+
+for question, answers in questions.items():
+    print(f"Pytanie: { question }")
+    for i, answer in enumerate(answers):
+        print(f"\t{i + 1} - {answer}")
+    user_answer = input("Podaj odpowiedź: ")
+    if not user_answer.isdigit():
+        print("Nie podałeś liczby!!!")
+        continue
+
+    answer_text = answers[int(user_answer) - 1]
+
+    for a in quiz_data:
+        if a["answer"] == answer_text:
+            if int(a["is_true"]):
+                print("****** Dobra odpowiedź *******")
+            else:
+                print("!!!!!! Zła odpowiedź !!!!!!")
